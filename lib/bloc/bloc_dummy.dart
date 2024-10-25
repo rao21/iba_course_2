b93381ff7a545e8261e7ef0ef451916908f69674
rao21 <rao.noman786@outlook.com>
2024-10-25 15:21:27 +0500

    boc dummy code.

---
+++ lib/bloc/bloc_dummy.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shimmer/flutter_shimmer.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// Product Model
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String thumbnail;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.thumbnail,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      price: json['price'].toDouble(),
+      thumbnail: json['thumbnail'],
+    );
+  }
+}
+
+// Bloc Event
+abstract class ProductEvent {}
+
+class FetchProducts extends ProductEvent {}
+class FetchProducts2 extends ProductEvent {}
+// Bloc State
+abstract class ProductState {}
+class ProductInitial extends ProductState {}
+class ProductLoading extends ProductState {}
+class ProductLoaded extends ProductState {
+  final List<Product> products;
+  ProductLoaded(this.products);
+}
+class ProductError extends ProductState {
+  final String error;
+  ProductError(this.error);
+}
+
+// Updated Bloc
+class ProductBloc extends Bloc<ProductEvent, ProductState> {
+  ProductBloc() : super(ProductInitial()) {
+    on<FetchProducts>(_onFetchProducts);
+    
+  }
+
+  Future<void> _onFetchProducts(
+    FetchProducts event,
+    Emitter<ProductState> emit,
+  ) async {
+    emit(ProductLoading());
+    try {
+      final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final products = (data['products'] as List)
+            .map((productJson) => Product.fromJson(productJson))
+            .toList();
+        emit(ProductLoaded(products));
+      } else {
+        emit(ProductError('Failed to load products'));
+      }
+    } catch (e) {
+      emit(ProductError('Error: $e'));
+    }
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product List',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        appBarTheme: const AppBarTheme(),
+        useMaterial3: false,
+      ),
+      home: BlocProvider(
+        create: (context) => ProductBloc()..add(FetchProducts()),
+        child: ProductScreen(),
+      ),
+    );
+  }
+}
+
+class ProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Products')),
+      body: BlocBuilder<ProductBloc, ProductState>(
+        builder: (context, state) {
+          if (state is ProductLoading) {
+            // Show a skeleton loading list when data is loading
+            return const SkeletonList();
+          } else if (state is ProductLoaded) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemCount: state.products.length,
+                itemBuilder: (context, index) {
+                  final product = state.products[index];
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    elevation: 4,
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            child: Image.network(
+                              product.thumbnail,
+                              height: 80,
+                              width: 80,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  product.title,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  product.description,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  '\$${product.price.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          } else if (state is ProductError) {
+            return Center(child: Text(state.error));
+          }
+          return Center(child: Text('Press button to fetch products'));
+        },
+      ),
+    );
+  }
+}
+
+class SkeletonList extends StatelessWidget {
+  const SkeletonList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => ListTileShimmer(
+        isDisabledAvatar: false,
+        isPurplishMode: true,
+        isRectBox: true,
+        colors: [Colors.grey[300]!, Colors.grey[100]!],
+        hasBottomBox: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:iba_course_2/lect1/list_view.dart';
 import 'package:iba_course_2/lect2/custom_tab.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
